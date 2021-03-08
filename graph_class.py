90809fc592ee4910d7fb71a5c0ed2079c904192d
oleedart <oscar.h.lee.23@dartmouth.edu>
2021-03-08 10:43:47 -0500

    Start at e. Not sure if my topological sort is correct. What I'm doing is keeping track of the vertexs we add to the stack and then reversing it per the suedocode that is in lecture notes

--- graph_class.py
+++ graph_class.py
@@ -1,9 +1,9 @@
-##### This is a class. It contains the methods for creating and 
+##### This is a class. It contains the methods for creating and
 ##### minor manipulations of graphs.
-##### 
+#####
 ##### The graph is stored as an adjacency list. This is represented by a dict() structure,
-##### where the keys are vertices and the values are sets with the corresponding neighborhood lists. 
-##### 
+##### where the keys are vertices and the values are sets with the corresponding neighborhood lists.
+#####
 ##### Original code by : C. Seshadhri, Jan 2015
 ##### Modified by : Deeparnab Chakrabarty, Apr 2019
 
@@ -11,17 +11,18 @@ import itertools
 import string
 import time as timer
 
+
 class graph(object):
 
-#### Initializing empty graph
-####
+    #### Initializing empty graph
+    ####
 
     # Globals
 
     def __init__(self):
-        self.vertices = set()    # Vertices are stored in a set   
-        self.adj_list = dict()   # Initial adjacency list is empty dictionary 
-        self.weights = dict()    # Initialize weights list for edges; default is 1
+        self.vertices = set()  # Vertices are stored in a set
+        self.adj_list = dict()  # Initial adjacency list is empty dictionary
+        self.weights = dict()  # Initialize weights list for edges; default is 1
         self.visited = dict()
         self.time = 0
         self.first = dict()
@@ -32,11 +33,10 @@ class graph(object):
         self.SCCs = list()
         self.num_SCC = 0
 
+    #### Stats
+    ####
 
-#### Stats
-####
-
-    def num_vertices(self):     #Returns number of vertices
+    def num_vertices(self):  # Returns number of vertices
         return len(self.vertices)
 
     def num_edges(self):
@@ -45,149 +45,141 @@ class graph(object):
             s = s + len(self.adj_list[v])
         return s
 
-#### Checks if (node) is vertex of graph. Output is 1 (yes) or 0 (no).
-####
+    #### Checks if (node) is vertex of graph. Output is 1 (yes) or 0 (no).
+    ####
 
-    def isVertex(self,node):
-        if node in self.vertices:               # Check if node is vertex
+    def isVertex(self, node):
+        if node in self.vertices:  # Check if node is vertex
             return 1
-        return 0                                 # Vertex not present!
+        return 0  # Vertex not present!
 
-#### Checks if (node1, node2) is edge of graph. Output is 1 (yes) or 0 (no).
-####
+    #### Checks if (node1, node2) is edge of graph. Output is 1 (yes) or 0 (no).
+    ####
 
-    def isEdge(self,node1,node2):
-        if node1 in self.vertices:               # Check if node1 is vertex
-            if node2 in self.adj_list[node1]:    # Then check if node2 is neighbor of node1
-                return 1                         # Edge is present!
+    def isEdge(self, node1, node2):
+        if node1 in self.vertices:  # Check if node1 is vertex
+            if node2 in self.adj_list[node1]:  # Then check if node2 is neighbor of node1
+                return 1  # Edge is present!
 
-#        if node2 in self.vertices:               # Check if node2 is vertex
-#            if node1 in self.adj_list[node2]:    # Then check if node1 is neighbor of node2 
-#                return 1                         # Edge is present!
+        #        if node2 in self.vertices:               # Check if node2 is vertex
+        #            if node1 in self.adj_list[node2]:    # Then check if node1 is neighbor of node2
+        #                return 1                         # Edge is present!
 
-        return 0                                 # Edge not present!
+        return 0  # Edge not present!
 
-##### Return weight of edge (u,v) if present. Assumed to be directed
-##### 
-      
+    ##### Return weight of edge (u,v) if present. Assumed to be directed
+    #####
+
     def weight(self, node1, node2):
         if self.isEdge(node1, node2):
-            return self.weights[(node1,node2)]
+            return self.weights[(node1, node2)]
         else:
-            print((node1,node2), "is not an edge")
+            print((node1, node2), "is not an edge")
             return None
 
-        
-    
-    
-#### Add vertex (v)
-####
+    #### Add vertex (v)
+    ####
 
-    def Add_Vertex(self,node):
+    def Add_Vertex(self, node):
         if not self.isVertex(node):
             self.vertices.add(node)
             self.adj_list[node] = set()
 
-#### Add directed edge (node1, node2). Can add weights as (node1, node2, w). Default w = 1
-####
-    
-    def Add_Edge(self,node1,node2,wt=1):
-        if node1 == node2:                  # Self loop, so do nothing
+    #### Add directed edge (node1, node2). Can add weights as (node1, node2, w). Default w = 1
+    ####
+
+    def Add_Edge(self, node1, node2, wt=1):
+        if node1 == node2:  # Self loop, so do nothing
             return
         if not self.isVertex(node1):
             self.Add_Vertex(node1)
         if not self.isVertex(node2):
             self.Add_Vertex(node2)
 
-        nbrs = self.adj_list[node1]   # nbrs is neighbor list of node1
-        if node2 not in nbrs:         # Check if node2 already neighbor of node1
-            nbrs.add(node2)           # Add node2 to this list
-        
-        self.weights[(node1,node2)] = wt    #add weight
+        nbrs = self.adj_list[node1]  # nbrs is neighbor list of node1
+        if node2 not in nbrs:  # Check if node2 already neighbor of node1
+            nbrs.add(node2)  # Add node2 to this list
 
-#### Add undirected, simple edge (node1, node2) Can add weights as (node1, node2, w). Default w = 1
-#### Calls above twice
-    
-    def Add_Und_Edge(self,node1,node2,w=1):
-        self.Add_Edge(node1,node2,w)
-        self.Add_Edge(node2,node1,w)
+        self.weights[(node1, node2)] = wt  # add weight
 
-        
-#### Read a graph from a file with list of edges. Arguments are fname (file name), sep (separator), flag (undirected/directed). Looks for file fname.
-#### Default is DIRECTED. Set flag = "u" for UNDIRECTED
-#### Assumes that line looks like:
-#### 
-#### node1 sep node2 sep weight
-####
-####
-#### If sep is not set, then it is just whitespace.
-#### IMPORTANT: if the first character of line is # (hash), this line is assumed to be a comment and ignored.
+    #### Add undirected, simple edge (node1, node2) Can add weights as (node1, node2, w). Default w = 1
+    #### Calls above twice
 
+    def Add_Und_Edge(self, node1, node2, w=1):
+        self.Add_Edge(node1, node2, w)
+        self.Add_Edge(node2, node1, w)
 
-    
-    
-    def Read_Edges(self,fname,sep=None,flag=None):
+    #### Read a graph from a file with list of edges. Arguments are fname (file name), sep (separator), flag (undirected/directed). Looks for file fname.
+    #### Default is DIRECTED. Set flag = "u" for UNDIRECTED
+    #### Assumes that line looks like:
+    ####
+    #### node1 sep node2 sep weight
+    ####
+    ####
+    #### If sep is not set, then it is just whitespace.
+    #### IMPORTANT: if the first character of line is # (hash), this line is assumed to be a comment and ignored.
+
+    def Read_Edges(self, fname, sep=None, flag=None):
         self.vertices = set()
-        self.adj_list = dict() #purge graph before reading
-        
-        f_input = open(fname,'r')        # Open file
-        list_edges = f_input.readlines() # Read lines as list
-        
-        for each_edge in list_edges:             # Loop of each line/edge
-            edge = each_edge.strip()             # Remove whitespace from edge
-            if len(edge) == 0:                   # If empty line, move to next line
+        self.adj_list = dict()  # purge graph before reading
+
+        f_input = open(fname, 'r')  # Open file
+        list_edges = f_input.readlines()  # Read lines as list
+
+        for each_edge in list_edges:  # Loop of each line/edge
+            edge = each_edge.strip()  # Remove whitespace from edge
+            if len(edge) == 0:  # If empty line, move to next line
                 continue
-            if edge[0] == '#':                   # line starts with #, and is comment
-                continue                         # this is comment, so move to next line
-            tokens = edge.split(sep)        # Split by sep to get tokens (nodes)
-            if(len(tokens) > 2):
+            if edge[0] == '#':  # line starts with #, and is comment
+                continue  # this is comment, so move to next line
+            tokens = edge.split(sep)  # Split by sep to get tokens (nodes)
+            if (len(tokens) > 2):
                 w = tokens[2]
             else:
                 w = 1
-            if(flag=="u"):                  #If flag set to undirected
-                self.Add_Und_Edge(tokens[0],tokens[1],w)
+            if (flag == "u"):  # If flag set to undirected
+                self.Add_Und_Edge(tokens[0], tokens[1], w)
             else:
-                self.Add_Edge(tokens[0],tokens[1],w) # Default is directed. Add directed edge given by first two tokens
-            
-#### Read labels on vertices of a graph
-#### Assumes that line looks like:
-#### 
-#### node sep label
-####
-#### If node not found in G.vertices, then will return an error
-####
-#### If sep is not set, then it is just whitespace.
-#### IMPORTANT: if the first character of line is # (hash), this line is assumed to be a comment and ignored.   
+                self.Add_Edge(tokens[0], tokens[1],
+                              w)  # Default is directed. Add directed edge given by first two tokens
+
+    #### Read labels on vertices of a graph
+    #### Assumes that line looks like:
+    ####
+    #### node sep label
+    ####
+    #### If node not found in G.vertices, then will return an error
+    ####
+    #### If sep is not set, then it is just whitespace.
+    #### IMPORTANT: if the first character of line is # (hash), this line is assumed to be a comment and ignored.
 
     def Read_Vertex_Labels(self, fname, sep=None):
         self.label = dict()
-        
-        f_input = open(fname,'r')        # Open file
-        list_pairs = f_input.readlines() # Read lines as list
-        
+
+        f_input = open(fname, 'r')  # Open file
+        list_pairs = f_input.readlines()  # Read lines as list
+
         for each_line in list_pairs:
-            pair = each_line.strip()    # Remove whitespace
-            if(len(pair) == 0):         #if empty line or start with #, ignore
+            pair = each_line.strip()  # Remove whitespace
+            if (len(pair) == 0):  # if empty line or start with #, ignore
                 continue
             if pair[0] == '#':
                 continue
             token = pair.split(sep)
-            if(self.isVertex(token[0])):
+            if (self.isVertex(token[0])):
                 self.label[token[0]] = token[1]
             else:
                 print("Error: Vertex not in Graph")
                 return -1
 
-#### 
-#### For some applications you may need the incidence lists to be ordered in a certain order sigma. The following code does that
-                
+    ####
+    #### For some applications you may need the incidence lists to be ordered in a certain order sigma. The following code does that
 
     def Reorder_Edges(self, sg):
         for v in self.vertices:
-            self.adj_list[v] = sorted(self.adj_list[v], key=lambda x:sg.index(x))
+            self.adj_list[v] = sorted(self.adj_list[v], key=lambda x: sg.index(x))
 
-
- #### Calculates maximum out degree
+    #### Calculates maximum out degree
     def max_degree(self):
         max = 0
         for v in self.vertices:
@@ -195,51 +187,64 @@ class graph(object):
                 max = len(self.adj_list[v])
         return max
 
-#### Depth First Search
+    #### Depth First Search
 
-    def DFS(self) :
+    def DFS(self):
 
         for v in self.vertices:
             self.visited[v] = 0
 
-        path = [] 
-        
+        global topstack
+        topstack = []
+        path = []
+
         for v in sorted(self.vertices):
             if self.visited[v] == 0:
                 self.DFSHelper(v, path)
+
 
         print("first", self.first)
         return path
 
     def DFSHelper(self, vertex, path):
+        global time
 
-        stack = [] 
-        stack.append(vertex) 
+        stack = []
+        stack.append(vertex)
 
-        
-        while len(stack) != 0: # not empty
+        while len(stack) != 0:  # not empty
             u = stack[-1]
             stack.pop()
-            if self.visited[u] == 0: # not visited
+            if self.visited[u] == 0:  # not visited
                 self.visited[u] = 1
                 path.append(u)
-            
+                #topstack.append(u)
+
+
                 # add all unvisited neighbors
-            for n in sorted(self.adj_list[u], reverse=True):
-                #print("i am vertex ", u, " and my neighbors are ", sorted(self.adj_list[u]))
-                #print(sorted(self.adj_list[u]), n)
-                if self.visited[n] == 0:
-                    stack.append(n)
+                for n in sorted(self.adj_list[u], reverse=True):
+                    # print("i am vertex ", u, " and my neighbors are ", sorted(self.adj_list[u]))
+                    # print(sorted(self.adj_list[u]), n)
+                    if self.visited[n] == 0:
+                        stack.append(n)
+                        #topstack.append(n)
+
+                topstack.append(u)
 
 
-    def DFSwithFirst(self) :
+
+
+
+        #print(topstack)
+
+    def DFSwithFirst(self):
         for v in self.vertices:
             self.visited[v] = 0
             self.first[v] = 0
             self.last[v] = 0
             self.root[v] = 0
             self.Fcomp[v] = 0
-        
+
         # print("sorted", sorted(self.vertices))
         for v in sorted(self.vertices):
             if self.visited[v] == 0:
@@ -247,35 +252,33 @@ class graph(object):
                 self.root[self.fcomp] = v
                 self.DFSwithFirstHelper(v)
         print(self.visited)
-        
+
         print(self.first)
         print(self.last)
-        
 
     def DFSwithFirstHelper(self, vertex):
 
-        # stack = [] 
-        # stack.append(vertex) 
+        # stack = []
+        # stack.append(vertex)
 
         self.visited[vertex] = 1
         self.Fcomp[vertex] = self.fcomp
         self.time += 1
         self.first[vertex] = self.time
-        
+
         for n in sorted(self.adj_list[vertex]):
             if self.visited[n] == 0:
                 # Add edge to forest
-                #self.Fcomp.Add_Edge(vertex, n)
+                # self.Fcomp.Add_Edge(vertex, n)
                 self.DFSwithFirstHelper(n)
-        
+
         self.time += 1
         self.last[vertex] = self.time
-        
 
     def DFS_SCC(self, isOrder, order):
         global time
 
-        if(isOrder):
+        if (isOrder):
             self.Reorder_Edges(order)
 
         for v in self.vertices:
@@ -284,23 +287,20 @@ class graph(object):
             self.last[v] = 0
             self.root[v] = 0
             self.Fcomp[v] = 0
-        
-        self.SCCs.append(0)
-        
-        time = 0 # reset counter
 
+        self.SCCs.append(0)
+
+        time = 0  # reset counter
 
         for v in sorted(self.vertices):
             if self.visited[v] == 0:
-                self.fcomp = self.fcomp + 1 # adds to tree count
-                self.root[self.fcomp] = v # sets root of current tree
+                self.fcomp = self.fcomp + 1  # adds to tree count
+                self.root[self.fcomp] = v  # sets root of current tree
 
                 self.num_SCC += 1
                 self.SCCs.append(list())
                 self.DFS_SCC_Helper(v)
 
-
-    
     def DFS_SCC_Helper(self, v):
         global time
         stack = []
@@ -309,23 +309,19 @@ class graph(object):
             time += 1
             u = stack[-1]
             self.Fcomp[u] = self.fcomp
-            if self.visited[u] == 0: # unvisisted
+            if self.visited[u] == 0:  # unvisisted
                 self.visited[u] = 1
                 self.first[u] = time
-                self.SCCs[self.num_SCC].append(u) # add vertex to current SCC
-                for n in sorted(self.adj_list[u], reverse=True): # reverse helps with stack ordering
-                    if self.visited[n] == 0: # unvisited
+                self.SCCs[self.num_SCC].append(u)  # add vertex to current SCC
+                for n in sorted(self.adj_list[u], reverse=True):  # reverse helps with stack ordering
+                    if self.visited[n] == 0:  # unvisited
                         stack.append(n)
-            else: # this vertex has been seen at least once and shouldn't be in stack
-                stack.pop() # remove from stack because already seen
-                if self.visited[u] == 1: # visited once
+            else:  # this vertex has been seen at least once and shouldn't be in stack
+                stack.pop()  # remove from stack because already seen
+                if self.visited[u] == 1:  # visited once
                     self.visited[u] = 2
                     self.last[u] = time
 
-        
-
-
-        
     def findSCC(self):
         global time
         # run DFS in forward order
@@ -337,17 +333,17 @@ class graph(object):
         for v in self.vertices:
             grev.Add_Vertex(v)
             for u in self.adj_list[v]:
-                grev.Add_Edge(u,v) 
-        
-        # run DFS again
+                grev.Add_Edge(u, v)
+
+                # run DFS again
         for v in grev.vertices:
             grev.visited[v] = 0
             grev.first[v] = 0
             grev.last[v] = 0
-        
+
         grev.SCCs.append([0])
-        
-        time = 0 # reset counter
+
+        time = 0  # reset counter
 
         # run in pi order
         while len(pi) != 0:
@@ -356,18 +352,37 @@ class graph(object):
                 grev.num_SCC += 1
                 grev.SCCs.append(list())
                 grev.DFS_SCC_Helper(v)
-    
+
         # return number of SCC and  num vertices in max SCC
         biggest = 0
         for scc in grev.SCCs:
             if len(scc) > biggest:
                 biggest = len(scc)
-            
+
         print("num of SCC: ", grev.num_SCC)
         print("biggest SCC: ", biggest)
-        
 
-    
+    def Top_Order(self):
+        #Run DFS
+        self.DFS()
+        return topstack[::-1]
+        # print(topstack[::-1])
+
+        #new_stack = []
+        # for n in topstack:
+        #     new_stack.append(int(n))
+        # sort = sorted(new_stack, reverse=True)
+        # print(sort)
+
+        # Re-order? Not sure if this works or not but what ever
+        #self.Reorder_Edges(path)
+
+
+
+    def Longest_Path(self):
+        sigma = self.Top_Order()
+        print(sigma)
+
 
 #### Problem 1a
 facebookGraph = graph()
@@ -389,7 +404,6 @@ graph3 = graph()
 #### Problem 1d
 graph2.findSCC()
 
-
 # start = timer.time()
 # facebookGraph.findSCC()
 # end = timer.time()
@@ -402,12 +416,15 @@ graph2.findSCC()
 # end = timer.time()
 # print("Time taken for epinions: ", end-start)
 
+#
+# google = graph()
+# google.Read_Edges("CS31/googlemaps.txt")
+# start = timer.time()
+# google.findSCC()
+# end = timer.time()
+# print("Time taken for google maps: ", end - start)
 
-google= graph()
-google.Read_Edges("googlemaps.txt")
-start = timer.time()
-google.findSCC()
-end = timer.time()
-print("Time taken for google maps: ", end-start)
-
-
+# Part e
+fb = graph()
+fb.Read_Edges("facebooksample.txt")
+fb.Longest_Path()
